75c2d83f393f1134cdb51efac4b96e8f687c25a2
Mehdi <aouidamehdi@gmail.com>
2020-09-15 16:36:31 +0200

    debut back page panier

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -6,17 +6,35 @@ use App\Entity\Panier;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Entity\Articles;
 
 class PanierController extends AbstractController
 {
+    private $session;
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
     /**
      * @Route("/panier", name="panier")
      */
     public function showpanier()
     {
-        $repository = $this->getDoctrine()->getRepository(Articles::class);
-        $articles = $repository->findAll();
+        $panier = $this->session->get('panier');
+        $articlesPanier = array();
+        dump($panier);
+        if ($panier != null) {
+            $articles = $panier->getArticles();
+        } else {
+            $articles = array();
+        }
+        foreach ($articles as $valueArticle) {
+            foreach ($articlesPanier as $valuePanier) {
+                if ($valuePanier == $valueArticle) {
+
+                }
+            }
+        }
         return $this->render('panier/index.html.twig', [
             'controller_name' => 'PanierController', 'articles' => $articles,
         ]);
