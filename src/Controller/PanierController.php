4a982342000a1842f9596d83b333d420af6ecf68
Your NameKITAZAT0 <kevinbrandaobeauchard@gmail.com>
2020-09-15 09:15:17 +0200

    visuel du tableau panier

--- src/Controller/PanierController.php
+++ src/Controller/PanierController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Articles;
 
 class PanierController extends AbstractController
 {
