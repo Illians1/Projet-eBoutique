6f03942c67dda941d3303bedf5ef3159e5585cd5
Your NameKITAZAT0 <kevinbrandaobeauchard@gmail.com>
2020-09-17 13:50:20 +0200

    modal view

--- migrations/Version20200916145514.php
+++ migrations/Version20200916145514.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200916142802 extends AbstractMigration
+final class Version20200916145514 extends AbstractMigration
 {
     public function getDescription() : string
     {
@@ -20,12 +20,14 @@ final class Version20200916142802 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE articles DROP vidArticle');
+        $this->addSql('DROP TABLE panier');
+        $this->addSql('ALTER TABLE articles DROP vidArticle, DROP nombre');
     }
 
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE articles ADD vidArticle VARCHAR(30) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`');
+        $this->addSql('CREATE TABLE panier (id INT AUTO_INCREMENT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8 COLLATE `utf8_unicode_ci` ENGINE = InnoDB COMMENT = \'\' ');
+        $this->addSql('ALTER TABLE articles ADD vidArticle VARCHAR(30) CHARACTER SET utf8 NOT NULL COLLATE `utf8_unicode_ci`, ADD nombre INT DEFAULT NULL');
     }
 }
